0e0aea81097e5e237b99817af09b0cd667690e81
Daniel Shaw <dan@MacBook-Pro.local>
2019-01-20 19:42:45 +0000

    App store apps, and better config sharing

--- setup.sh
+++ setup.sh
@@ -6,10 +6,15 @@ brew tap mas-cli/tap
 brew tap-pin mas-cli/tap
 brew install mas
 
+mas install 803453959 # Slack
+mas install 1333542190 # 1password 7
+mas install 918858936 # Airmail 3
+
 # Keys
 mkdir ~/.ssh/
-cp ~/Dropbox/config ~/.ssh/
+ln -s ~/Dropbox/Apps/sshconfig ~/.ssh/config
+
 
 # Terminal
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp ~/Dropbox/zshrc ~/.zshrc
+ln -s ~/Dropbox/Apps/zshrc ~/.zshrc
