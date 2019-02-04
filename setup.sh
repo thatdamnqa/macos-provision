1fea62755c240375a4572d4451f6d40f4249e99d
Daniel Shaw <dan@MacBook-Pro.local>
2019-02-04 21:05:56 +0000

    More appstore apps

--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@ brew install mas
 mas install 803453959 # Slack
 mas install 1333542190 # 1password 7
 mas install 918858936 # Airmail 3
+mas install 406056744 # Evernote
+mas install 585829637 # Todoist
 
 # Keys
 mkdir ~/.ssh/
